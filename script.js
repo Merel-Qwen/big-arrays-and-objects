64c4773e4fd581295f677e09f5bd16cff0a4a304
Merel-Qwen <58332874+Merel-Qwen@users.noreply.github.com>
2020-03-20 10:46:16 +0100

    Select on region

--- script.js
+++ script.js
@@ -15,11 +15,16 @@ const addRegionsToDom = function() {
   const sortedNames = uniqueRegionNames.sort();
   console.log(sortedNames);
   const listItems = sortedNames.map(function(item) {
+    console.log(item);
     const listItem = document.createElement("li");
+    listItem.innerText = item;
+
+    //console.log(listItem);
     return listItem;
   });
 
   listItems.forEach(function(item) {
     regionList.appendChild(item);
+    console.log(item);
   });
 };
